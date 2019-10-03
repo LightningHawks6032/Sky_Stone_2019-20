35d7abdaedc3d2db00ff96e95c3821ba7d0bc6cc
Mich-T <mitoryu@students.natickps.org>
2019-10-03 15:54:50 -0400

    started weekendbot hardware class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/WeekendBot_Hardware.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class WeekendBot_Hardware implements  RobotHardware{
+    public 
+}
