a34635b48b8827a5ca6f46ad40cb4e6c8b548807
Mich-T <mitoryu@students.natickps.org>
2019-11-05 16:30:58 -0500

    messing with angle stuff!
    includes new theta determining method framework and starting angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WeekendBot/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WeekendBot/Auto.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.WeekendBot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.FieldMapping.FieldElement;
 import org.firstinspires.ftc.teamcode.FieldMapping.FieldMap;
+import org.firstinspires.ftc.teamcode.FieldMapping.Vector;
 import org.firstinspires.ftc.teamcode.Hardware.WeekendBot_Hardware;
 
 
@@ -10,6 +12,7 @@ public class Auto {
     private WeekendBot_Hardware hardware;
     public FieldMap fieldMap = new FieldMap();
     private long startTime;
+    public Vector startImage;
 
     public Auto(LinearOpMode auto, WeekendBot_Hardware hardware){
         autonomous = auto;
@@ -25,6 +28,54 @@ public class Auto {
         hardware.drivetrain.setStartTime(time);
         hardware.intake.setStartTime(time);
         hardware.outtake.setStartTime(time);
+    }
+
+
+    //sets the start position of the robot dependent
+    public void setStartPosition(int quadrant) throws InterruptedException{
+
+        Vector startPos;
+
+        switch (quadrant) {
+            case 1:
+                startPos = fieldMap.get(FieldElement.BPOS2);
+                hardware.drivetrain.setRobotPos(startPos);
+                startImage = fieldMap.get(FieldElement.IMAGE6);
+                break;
+            case 2:
+                startPos = fieldMap.get(FieldElement.BPOS1);
+                hardware.drivetrain.setRobotPos(startPos);
+                startImage = fieldMap.get(FieldElement.IMAGE4);
+                break;
+            case 3:
+                startPos = fieldMap.get(FieldElement.RPOS1);
+                hardware.drivetrain.setRobotPos(startPos);
+                startImage = fieldMap.get(FieldElement.IMAGE3);
+                break;
+            case 4:
+                startPos = fieldMap.get(FieldElement.RPOS2);
+                hardware.drivetrain.setRobotPos(startPos);
+                startImage = fieldMap.get(FieldElement.IMAGE5);
+                break;
+        }
+
+    }
+
+    //for simplicity of code, this method assumes that the robot is facing a nav target
+    //facing: own wall by default; in method with facing as parameter:
+    //                      1: Judges/non-audience 2: Blue side 3: Audience 4: Red side
+    public int startAngle(int quadrant){
+        if(quadrant == 1 || quadrant == 2){
+            return 90;
+        }
+        return 270;
+    }
+
+    public int startAngle(int quadrant, int facing){
+        if (quadrant == 1 || quadrant == 2){
+            if(facing == 1) ;
+        }
+        return 0;
     }
 
 
