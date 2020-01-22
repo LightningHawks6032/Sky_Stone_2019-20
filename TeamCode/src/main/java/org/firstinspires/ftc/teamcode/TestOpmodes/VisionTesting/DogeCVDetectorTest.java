e58b98192eddb4e98c210d525f50462bcb31a9d5
Mich-T <mitoryu@students.natickps.org>
2020-01-22 12:21:40 -0500

    Added code to dogecv detector

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpmodes/VisionTesting/DogeCVDetectorTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.TestOpmodes.VisionTesting;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class DogeCVDetectorTest extends LinearOpMode {
+    public void runOpMode(){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DogeCVDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DogeCVDetector.java
@@ -4,25 +4,31 @@ import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 
 public class DogeCVDetector {
     // DogeCV/OpenCV declarations
     private OpenCvCamera phoneCam;
     private SkystoneDetector skyStoneDetector;
+    private double robotCamX, robotCamY;
 
-    private double robotCenterX, robotCenterY;
+    private final double LEFT_MARGIN = 0, RIGHT_MARGIN = 100, CONVERSION_FACTOR = .5;
 
 
     public DogeCVDetector(double centerX, double centerY, HardwareMap hardwareMap){
-        robotCenterX = centerX;
-        robotCenterY = centerY;
+        robotCamX = centerX*CONVERSION_FACTOR;
+        robotCamY = centerY*CONVERSION_FACTOR;
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
     }
 
     public void initCameraConnection(){
         phoneCam.openCameraDevice();
+
+        skyStoneDetector = new SkystoneDetector();
+        phoneCam.setPipeline(skyStoneDetector);
+
     }
 
 
@@ -31,12 +37,18 @@ public class DogeCVDetector {
      * 0 (left), 1 (center), or 2 (right)
      * Alliance parameter: 1 (red), 2 (blue). These are the same values as the AutonomousData class
      */
-    public int detectSkyStoneAlign(int alliance){
-        int result = 0; //will default to center if no value is detected
+    double debugLocation = 0;
 
+    public int detectSkyStoneAlign(int alliance) throws InterruptedException{
+        int result = 1; //will default to center if no value is detected
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+        Thread.sleep(4000);
 
+        double location = skyStoneDetector.getScreenPosition().x + robotCamX;
+        debugLocation = location;
 
-
+        if(location > RIGHT_MARGIN) result = 2;
+        else if(location < LEFT_MARGIN) result = 0;
 
         return result;
     }
